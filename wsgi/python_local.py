dc3fe9ab2b688c9c82d424ee7f723bffaa768b5e
usuario <usuario@debian>
2016-05-12 19:46:48 +0200

    eliminado fichero corrupto

--- wsgi/python_local.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from bottle import run
-import os
-os.environ['OPENSHIFT_REPO_DIR']=""
-import mybottleapp
-run(host='0.0.0.0', port=8080, debug=True)
