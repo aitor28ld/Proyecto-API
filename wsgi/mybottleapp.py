1f4d64041f1cdb404b73be4ee68eaed5f37fd2d3
root <root@debian>
2016-05-19 17:45:51 +0200

    Modificaciones realizadas en mybottleapp.py

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -1,6 +1,5 @@
 from bottle import Bottle, route, run, request, template, default_app, static_file, get, post, response, redirect 
 import requests
-from requests_oauthlib import OAuth1
 from requests_oauthlib import OAuth2Session
 from oauthlib.oauth2 import TokenExpiredError
 from urlparse import parse_qs
