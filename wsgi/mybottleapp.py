5b42f70c37864dac9118b398cd163d8136683bb4
root <root@debian>
2016-05-19 18:56:10 +0200

    Modificado fichero mybottleapp.py

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -93,6 +93,7 @@ def server_static(filepath):
 # This must be added in order to do correct path lookups for the views
 import os
 from bottle import TEMPLATE_PATH
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/views/')) 
 
 application=default_app()
