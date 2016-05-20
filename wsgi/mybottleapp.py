0619af82c0fa7ce57c62f1b6670c56831121ada9
aitor28ld <ldaitor28@gmail.com>
2016-05-20 10:44:08 +0200

    Modificado fichero mybottleapp.py para pruebas

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -48,7 +48,7 @@ def get_token():
 @get('/perfil')
 def personal():
 	token=request.get_cookie("token", secret='some-secret-key')
-	headers = {"Accept":"aplication/json","Authorization":token}
+	headers = {"Accept":"aplication/json","Authorization":"Bearer "token}
 	if token_valido():
 		token=request.get_cookie("token", secret=client_secret)
 		oauth2 = OAuth2Session(client_id, token=token)
