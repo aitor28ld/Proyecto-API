2dacef4797094a7b8494f3664f6c82e1ba5b7a22
aitor28ld <ldaitor28@gmail.com>
2016-05-20 12:59:57 +0200

    Modificado fichero perfil.tpl para pruebas

---
+++ wsgi/local.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from bottle import run
+import os
+os.environ['OPENSHIFT_REPO_DIR']=""
+import mybottleapp
+run(host='0.0.0.0', port=8080, debug=True)
