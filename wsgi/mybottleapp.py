513b96eebd04b1c9a53ec7a1bf5a7a143cc50bf7
aitor28ld <ldaitor28@gmail.com>
2016-05-23 12:43:10 +0200

    Error corregido

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -56,7 +56,7 @@ def personal():
 		
 	return template('perfil.tpl', perfil=cuenta)
 
-@get('/lista')
+@post('/lista')
 def lista():
 	nombreid = request.forms.get('creador')
 	nombrepl = request.forms.get('name')
